84d775e18f9f541e360ad2f1fab621ea9193e488
Edward Stewart <eddstewart1@hotmail.co.uk>
2024-03-18 00:08:21 +0000

    add confetti on win

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,12 +1,14 @@
 "use client";
 
 import {useGameContext} from "@/context/GameContext";
+import Confetti from "react-confetti";
 
 export default function GameOver() {
   const {winningPlayerName} = useGameContext();
 
   return (
     <div>
+      <Confetti run></Confetti>
       <p>GAME OVER</p>
       <p>{winningPlayerName} is the Winner! </p>
     </div>
